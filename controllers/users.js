3532000714743c282e7bc4a9ed569886e71de4f4
Anthony Shalagin <anthony.shalagin@gmail.com>
2016-09-28 10:29:48 -0400

    Added stub controllers

---
+++ controllers/users.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const models = require('../models');
+
+module.exports = {
+  registerRouter() {
+    const router = express.Router();
+
+    router.get('/', this.index);
+    router.get('/:username', this.show);
+
+    return router;
+  },
+  index(req, res) {
+    models.User.findAll().then((user) => {
+      res.render('users', {
+        user,
+      });
+    });
+  },
+  show(req, res) {
+    models.User.findOne({
+      where: { username: req.params.username },
+    }).then((user) => {
+      models.Post.findAll({
+        where: { email: user.email },
+      }).then((post) => {
+        res.render('users/single', {
+          user,
+          post,
+        });
+      });
+    }).catch(() => {
+      res.render('users/single');
+    });
+  },
+};
