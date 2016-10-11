e569c30fc626189602ebfc89775e7f96fc4a905a
MD R. Islam <mdislamwork@gmail.com>
2016-10-11 19:39:59 +0000

    added CRUD empty routes for transaction

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');             //Requiring routes (AS)
 var users = require('./routes/users');
+var transaction = require('./routes/transaction');
 
 var app = express();                                //Loading the express app (AS)
 
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);                               //Where we tell our routes to be used (AS)
 app.use('/users', users);
+app.use('/transaction', transaction);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {                  // 404 handling (AS)
