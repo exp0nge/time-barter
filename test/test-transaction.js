e1b518ab3c65f86e95445ef0f79f219494662921
MD R. Islam <mdislamwork@gmail.com>
2016-10-12 17:32:40 +0000

    added basic unit test

--- test/test-transaction.js
+++ test/test-transaction.js
@@ -6,7 +6,14 @@ var should = chai.should();
 chai.use(chaiHttp);
 
 describe('Transaction', function(){
-  it('should list all transactions for authenticated user on /transaction GET');
+  it('should list all transactions for authenticated user on /transaction GET', (done) => {
+    chai.request(server)
+      .get('/transaction')
+      .end((err, res) => {
+        res.should.have.status(200);        
+        done();
+      });
+  });
   it('should create a transaction for authenticated user on /transaction/create POST');
   it('should retrieve transaction for authenticated user on /transaction/:id GET');
   it('should update transaction for authenticated user on /transaction/update/:id PUT');
