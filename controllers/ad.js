13020c3c2e2e66963fb59106c7523b048b395d51
SupaStuff <StuffQilla@gmail.com>
2016-11-08 23:43:12 -0500

    tabs

--- controllers/ad.js
+++ controllers/ad.js
@@ -12,29 +12,30 @@ DELETE /:id/delete
 */
 
 module.exports = {
-  registerRouter() {
-    const router = express.Router();
+	registerRouter() {
+		const router = express.Router();
 
-    router.post('/new', this.new);
-    router.get('/:id', this.id);
-    router.get('/');
-    router.put('/:id/update', this.update);
-    router.put('/:id/deactivate', this.deactivate);
-    router.put('/:id/activate', this.activate);
-    router.delete('/:id/delete', this.delete);
-    return router;
-  },
+		router.post('/new', this.new);
+		router.get('/:id', this.id);
+		router.get('/');
+		router.put('/:id/update', this.update);
+		router.put('/:id/deactivate', this.deactivate);
+		router.put('/:id/activate', this.activate);
+		router.delete('/:id/delete', this.delete);
+		return router;
+	},
 
-  new(req, res) {
-    models.Ad.new({
-      username: req.Ad.username,
-      title: req.Ad.title,
-      description: req.Ad.description,
-      email: req.Ad.username
-    });
-  },
+	new(req, res) {
+		models.Ad.new({
+			username: req.Ad.username,
+			title: req.Ad.title,
+			description: req.Ad.description,
+			email: req.Ad.username
+		});
+	},
 
-  id(req, res) {
-    res.render('Ad/:id');
-  }
+	id(req, res) {
+		res.render('Ad/:id');
+	},
+
 };
