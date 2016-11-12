70ced2aa3c72cf83af6e290a3b36826c05e455bb
exp0nge <mdislamwork@gmail.com>
2016-11-12 15:50:47 -0500

    Added placeholder posts

--- app.js
+++ app.js
@@ -5,14 +5,15 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
 var app = express();                                //Loading the express app (AS)
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));    //Setting default views to the view directory (AS)
 app.set('view engine', 'hjs');                      //Setting view engine to use hogan templates (AS)
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.png')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,10 +24,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 //Use express as middleware to serve static files
 //Serve up files that are in the public directory and use Express's static file handler to do so
 app.use('/public', express.static("public"));
-
-app.get('/favicon.ico', function(req, res) {
-    res.sendStatus(200);
-});
 
 //Handlebars config
 var exphbs = require('express3-handlebars');
